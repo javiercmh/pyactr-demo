1e7029f158d2a6f1d9803633af2a6430a123a93f
Javier Carrasco Melo <javiercmh@gmail.com>
2021-12-16 16:12:24 +0100

    lisp file to adapt

---
+++ broken-addition.lisp
@@ -0,0 +1,93 @@
+
+(clear-all)
+
+(define-model addition
+
+(sgp :esc t :lf .05)
+
+(chunk-type number number next)
+(chunk-type add arg1 arg2 sum count)
+
+(add-dm
+ (zero isa number number zero next one)
+ (one isa number number one next two)
+ (two isa number number two next three)
+ (three isa number number three next four)
+ (four isa number number four next five)
+ (five isa number number five next six)
+ (six isa number number six next seven)
+ (seven isa number number seven next eight)
+ (eight isa number number eight next nine)
+ (nine isa number number nine next ten)
+ (ten isa number number ten)
+ (test-goal ISA add arg1 zero arg2 ten))
+
+(P initialize-addition
+   =goal>
+      arg1        =num1
+      arg2        =num2
+      sum         nil
+  ==>
+   =goal>
+      ISA         add
+      sum         =num1
+      count       zero
+   +retrieval>
+      ISA         number
+      number      =num1
+)
+
+(P terminate-addition
+   =goal>
+      ISA         add
+      count       =num
+      arg2        =num
+      sum         =answer
+  ==>
+   =goal>
+      ISA         add
+      count       nil
+)
+
+(P increment-count 
+   =goal>
+      ISA         add
+      sum         =sum
+      count       =count
+   =retrieval>
+      ISA         number
+      number      =count
+      next        =newcount
+==>
+   =goal>
+      ISA         add
+      count       =newcount
+   +retrieval>
+      ISA        number
+      number     =sum
+)
+
+(P increment-sum
+   =goal>
+      ISA         add
+      sum         =sum
+      count       =count
+    - arg2        =count
+   =retrieval>
+      ISA         number
+      number      =sum
+      next        =newsum
+==>
+   =goal>
+      ISA         add
+      sum         =newsum
+   +retrieval>
+      ISA         number
+      number      =count
+   
+)
+
+(goal-focus test-goal)
+
+)
+
